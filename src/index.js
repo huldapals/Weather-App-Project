646dab63d5e73a96f70729182ca6e69f6b9a5163
huldapals <huldapals94@gmail.com>
2021-04-27 10:19:50 +0200

    displayForecast API

--- src/index.js
+++ src/index.js
@@ -51,7 +51,8 @@ document.querySelector("#faranheit-button").addEventListener("click", function(e
 });
 
 //forecast
-function displayForecast(){
+function displayForecast(response){
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -77,6 +78,14 @@ function displayForecast(){
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey = "d803ee58ce516713db5656619dac775a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+
  function showTemperature(response) {
    temperature = Math.round(response.data.main.temp);
     document.querySelector("#cityHeading").innerHTML = response.data.name;
@@ -89,6 +98,8 @@ function displayForecast(){
     let iconElement = document.querySelector("#icon");
     iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
+  
+  getForecast(response.data.coord);
   }
 
 
@@ -121,5 +132,5 @@ navigator.geolocation.getCurrentPosition(showPosition);
 let pinButton = document.querySelector("#submitPin");
 pinButton.addEventListener("click", getCurrentLocation);
 
-displayForecast();
+
 

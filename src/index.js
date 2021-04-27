bf894dc9ee1ce7df8bef8fe5213740e89f28cdd6
huldapals <huldapals94@gmail.com>
2021-04-27 11:37:41 +0200

    forecast API Integration

--- src/index.js
+++ src/index.js
@@ -51,27 +51,39 @@ document.querySelector("#faranheit-button").addEventListener("click", function(e
 });
 
 //forecast
+function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["sun", "Mon","Tue","Wed","Thu","Fri","Sat"];
+
+return days[day];
+}
+
 function displayForecast(response){
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function(day){
+  forecast.forEach(function(forecastDay, index){
+    if (index < 5) {
     forecastHTML = forecastHTML + 
         `
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
             <img 
-              src="" 
+              src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
               class="iconForecast" 
-              alt="" />
+              alt="" 
+              width="70"
+              />
             <p>
-              <span class="highTemp">14°</span>
-              <span class="lowTemp"> | 2°</span>
+              <strong><span class="highTemp">${Math.round(forecastDay.temp.max)}°</span></strong>
+              <span class="lowTemp"> | ${Math.round(forecastDay.temp.min)}°</span>
             </p>
           </div>
         `;
+    }
   })
  
     forecastHTML = forecastHTML + `</div>`;
@@ -79,10 +91,8 @@ function displayForecast(response){
 }
 
 function getForecast(coordinates){
-  console.log(coordinates);
   let apiKey = "d803ee58ce516713db5656619dac775a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
 axios.get(apiUrl).then(displayForecast);
 }
 
